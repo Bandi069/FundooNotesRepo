d8d049f15419662ad1be5c89d5f71fc7b15cbfa4
Bandi069 <bandivenu89@gmail.com>
2020-04-11 18:32:49 +0530

    [Venu]Refactor.Search Bar adjustment in Scss

--- src/app/Component/dashboard/dashboard.component.ts
+++ src/app/Component/dashboard/dashboard.component.ts
@@ -11,11 +11,20 @@ import {ChangeDetectorRef, OnDestroy} from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  
+  mobileQuery: MediaQueryList;
 
-  constructor() { }
-
-  ngOnInit() {
+  fillerNav = Array.from({length: 5}, (_, i) => `Nav Item ${i + 1}`);
+  private _mobileQueryListener: () => void;
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
+  ngOnInit() {}
+  ngOnDestroy():void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
 
 }
