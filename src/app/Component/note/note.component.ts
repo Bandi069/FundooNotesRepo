1c372ac982f5da88869e97c354815c1327fd8f44
Bandi069 <bandivenu89@gmail.com>
2020-04-11 18:37:42 +0530

    [Venu]Refactor.Adding Note Component

---
+++ src/app/Component/note/note.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+
+@Component({
+  selector: 'app-note',
+  templateUrl: './note.component.html',
+  styleUrls: ['./note.component.scss']
+})
+export class NoteComponent implements OnInit {
+  title;
+  description;
+  card1 = true;
+  card2 = false;
+  constructor( ) 
+    { }
+
+  ngOnInit() {
+  }
+
+  note(){
+    this.card2=true;
+    this.card1=false;
+  }
+}
