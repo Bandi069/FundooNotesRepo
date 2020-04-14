1d82ecec010b78d1f1dc66a386ef29b7de5891cc
Bandi069 <bandivenu89@gmail.com>
2020-04-14 17:06:52 +0530

    [Venu]Refactor.implementation of Note TS component

--- src/app/Component/note/note.component.ts
+++ src/app/Component/note/note.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
+ import { NoteService } from 'src/app/Services/note.service';
+ import {DataSharingService} from 'src/app/Services/datasharing.service';
 
 @Component({
   selector: 'app-note',
@@ -7,15 +9,36 @@ import { MatSnackBar } from '@angular/material';
   styleUrls: ['./note.component.scss']
 })
 export class NoteComponent implements OnInit {
+  userData=JSON.parse(localStorage.getItem('userData'));
   title;
   description;
   card1 = true;
   card2 = false;
-  constructor(){ }
+  constructor(
+    private dataSharing:DataSharingService,
+    private notes:NoteService,  
+    private snackbar: MatSnackBar
+  ){ }
   ngOnInit() {
   }
+ 
   note(){
-    this.card2=true;
-    this.card1=false;
-  }
+        this.card2=true;
+        this.card1=false;
+      }
+      
+      close(){
+        this.card1 = true;
+        this.card2 = false;
+        if(this.title != null || this.description !=null){
+          this.notes.addNote(this.title ,this.description,this.userData.email).subscribe((status)=>{
+            if(status != null){
+              localStorage.setItem('noteData', JSON.stringify(status));
+              this.dataSharing.changeMessage(true);
+              this.snackbar.open('Note added.','', {duration: 2000});
+            }
+          });
+        }
+      }
 }
+
