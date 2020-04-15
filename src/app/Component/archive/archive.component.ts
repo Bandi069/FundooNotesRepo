6ed45a912de6fe0508d51c649c20596bf91bd797
Bandi069 <bandivenu89@gmail.com>
2020-04-15 12:24:13 +0530

    [Venu]Refactor.implemenation of ts in archive component

--- src/app/Component/archive/archive.component.ts
+++ src/app/Component/archive/archive.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NoteService } from 'src/app/Services/note.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DataSharingService } from 'src/app/Services/datasharing.service';
 
 @Component({
   selector: 'app-archive',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./archive.component.scss']
 })
 export class ArchiveComponent implements OnInit {
-
-  constructor() { }
-
+  notes:any=[];
+  Archive:string="row wrap";
+  email:any=[];
+  constructor(private serviceobj:NoteService,private bar:MatSnackBar,private datascharing:DataSharingService,private dialog:MatDialog) { }
   ngOnInit() {
+    this.getarchive();
+    this.datascharing.Message.subscribe(msg=>this.Archive=msg);
   }
-
+  getarchive(){
+    this.serviceobj.getArchive(email).subscribe(data=>{
+      console.log("Archive Note",data);
+      this.notes=data['result'];
+    },error=>{
+      console.log(error);
+    })
+ }
 }
