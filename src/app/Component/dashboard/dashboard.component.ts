97f218b151c2b36d5960ae4e837fd656ba5242e8
Bandi069 <bandivenu89@gmail.com>
2020-04-18 10:00:32 +0530

    [Venu]Refactor.changes in dasboard TS file

--- src/app/Component/dashboard/dashboard.component.ts
+++ src/app/Component/dashboard/dashboard.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
-import {MediaMatcher} from '@angular/cdk/layout';
-import {ChangeDetectorRef, OnDestroy} from '@angular/core';
-import { MatSidenavModule } from '@angular/material/sidenav'; 
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { Route } from '@angular/compiler/src/core';
 import { DataSharingService } from 'src/app/Services/datasharing.service';
 import { NoteService } from 'src/app/Services/note.service';
@@ -15,27 +15,33 @@ import { NoteService } from 'src/app/Services/note.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  toggle = true;
+  status = 'Enable';
+
   mobileQuery: MediaQueryList;
-  fillerNav = Array.from({length: 6}, (_, i) => `Nav Item ${i + 1}`);
+  fillerNav = Array.from({ length: 6 }, (_, i) => `Nav Item ${i + 1}`);
   private _mobileQueryListener: () => void;
 
   constructor(
-    private datasharing:DataSharingService,
-    private route:Router,
-    private serviobj:NoteService,
-    public dialog:MatDialog,
-    media:MediaMatcher,
-    Detector:ChangeDetectorRef) {
+    private datasharing: DataSharingService,
+    private route: Router,
+    private serviobj: NoteService,
+    public dialog: MatDialog,
+    media: MediaMatcher,
+    Detector: ChangeDetectorRef) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => Detector.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
   }
-ngOnInit()
-{
-} 
+  ngOnInit() {
+  }
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
   }
- shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
- 
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
+
+  enableDisableRule() {
+    this.toggle = !this.toggle;
+    this.status = this.toggle ? 'Enable' : 'Disable';
+  }
 }
