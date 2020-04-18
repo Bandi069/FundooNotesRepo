535b015fd5eb4bda9680c3e277e138f912808ff3
Bandi069 <bandivenu89@gmail.com>
2020-04-18 10:04:56 +0530

    [Venu]Refactor.implementation of display scss,ts file

--- src/app/Component/displaynote/displaynote.component.ts
+++ src/app/Component/displaynote/displaynote.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NoteService } from 'src/app/Services/note.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-displaynote',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./displaynote.component.scss']
 })
 export class DisplaynoteComponent implements OnInit {
-
-  constructor() { }
+  notepara: any;
+  email:any;
+  allnotes:any;
+  constructor(
+    private route:Router,
+    private noteser:NoteService,
+   
+  ) { }
 
   ngOnInit() {
+    // this.getNotes();
+    // this.route.queryParams.subscribe(params => {
+    //   this.notepara = params['page'];
+    //  // this.gridView=  params['view'];
+    // });
   }
-
+  getNotes(){
+    this.noteser.getNote(this.email).subscribe(data => {
+      console.log(data);
+      this.allnotes=data;
+     
+    });
+      }
 }
