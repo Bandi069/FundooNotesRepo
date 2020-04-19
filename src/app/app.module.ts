b153043c772cd1be034215bc5f0ca9b257c00f53
Bandi069 <bandivenu89@gmail.com>
2020-04-20 02:19:29 +0530

    [venu]Refactor.Changes in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 //import { NgModule } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
-import {MatFormFieldModule} from '@angular/material/form-field'
+import { MatFormFieldModule } from '@angular/material/form-field'
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {  MatButtonModule, MatInputModule, MatRippleModule} from '@angular/material';
-import {MatTooltipModule} from '@angular/material/tooltip';
-
+import { MatButtonModule, MatInputModule, MatRippleModule } from '@angular/material';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import {MatSnackBarModule} from '@angular/material/snack-bar'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './Component/register/register.component';
@@ -18,6 +18,22 @@ import { ForgotPasswordComponent } from './Component/forgot-password/forgot-pass
 import { ResetPasswordComponent } from './Component/reset-password/reset-password.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardComponent } from './Component/dashboard/dashboard.component';
+import { NoteComponent } from './Component/note/note.component';
+import { IconsComponent } from './Component/icons/icons.component';
+import {MatDialogModule, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { DisplaynoteComponent } from './Component/displaynote/displaynote.component'; 
+//import { HttpModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { AccountService } from './Services/account.service';
+import { NoteService } from './Services/note.service';
+import { ArchiveComponent } from './Component/archive/archive.component';
+import { ReminderComponent } from './Component/reminder/reminder.component';
+import { GetnoteComponent } from './Component/getnote/getnote.component';
+import { CardComponent } from './Component/card/card.component';
+import { TrashComponent } from './Component/trash/trash.component';
+
+ 
 
 @NgModule({
   declarations: [
@@ -26,7 +42,15 @@ import { DashboardComponent } from './Component/dashboard/dashboard.component';
     LoginComponent,
     ForgotPasswordComponent,
     ResetPasswordComponent,
-    DashboardComponent
+    DashboardComponent,
+    NoteComponent,
+    IconsComponent,
+    DisplaynoteComponent,
+    ArchiveComponent,
+    ReminderComponent,
+    GetnoteComponent,
+    CardComponent,
+    TrashComponent
   ],
   imports: [
     BrowserModule,
@@ -35,19 +59,22 @@ import { DashboardComponent } from './Component/dashboard/dashboard.component';
     BrowserAnimationsModule,
     FormsModule,
     MatMenuModule,
+    HttpClientModule,
+     //   HttpModule,
     //FormControl,
-   //FormGroup,
-   ReactiveFormsModule,
+    //FormGroup,
+    ReactiveFormsModule,
     MatInputModule,
+    MatDialogModule,
     MatButtonModule,
     MatTooltipModule,
     MatCardModule,
     MatIconModule,
+    MatSidenavModule,
+    MatSnackBarModule,
     RouterModule.forRoot([])
-
-
   ],
-  providers: [],
+  providers: [AccountService,NoteService],
   bootstrap: [AppComponent],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA,
