70d14bd5bd700ed4fc155ae4a87b4a62d24992ee
Bandi069 <bandivenu89@gmail.com>
2020-04-20 02:10:29 +0530

    [venu]Refactor.Implementation of Archive Component

--- src/app/Component/archive/archive.component.ts
+++ src/app/Component/archive/archive.component.ts
@@ -19,34 +19,34 @@ export class ArchiveComponent implements OnInit {
     private datascharing: DataSharingService,
     private dialog: MatDialog) { }
   ngOnInit() {
-    this.getarchive(this.id);
+   // this.getarchive(this.id);
     // this.datascharing.Message.subscribe(msg=>this.Archive=msg);
   }
-  getarchive(id) {
+  // getarchive(id) {
 
-    this.service.addArchive(id).subscribe(data => {
-      console.log("Archive Note", data);
-      this.notes = data['result'];
-    }, error => {
-      console.log(error);
-    })
-  }
-  unarchive(note) {
-    console.log(note);
-    try {
-      if (note.id == undefined || note.id == "") throw "note"
-      var results = {
-        'id': note.id,
-      }
-      this.service.unArchive(results).subscribe(data => {
-        this.bar.open("Unarchived", "", { duration: 2000 });
-        this.getarchive(this.id);
-      }, error => {
-        this.bar.open("not Unarchive", "", { duration: 2000 });
-      })
-    } catch (arr) {
-      this.bar.open(arr, "", { duration: 2000 });
-    }
-  }
+  //   this.service.addArchive(id).subscribe(data => {
+  //     console.log("Archive Note", data);
+  //     this.notes = data['result'];
+  //   }, error => {
+  //     console.log(error);
+  //   })
+  // }
+  // unarchive(note) {
+  //   console.log(note);
+  //   try {
+  //     if (note.id == undefined || note.id == "") throw "note"
+  //     var results = {
+  //       'id': note.id,
+  //     }
+  //     this.service.unArchive(results).subscribe(data => {
+  //       this.bar.open("Unarchived", "", { duration: 2000 });
+  //       this.getarchive(this.id);
+  //     }, error => {
+  //       this.bar.open("not Unarchive", "", { duration: 2000 });
+  //     })
+  //   } catch (arr) {
+  //     this.bar.open(arr, "", { duration: 2000 });
+  //   }
+  // }
 }
 
