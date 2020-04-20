95bdbfdd10e4a98921e6f9929b4e14a04c0f20c5
Bandi069 <bandivenu89@gmail.com>
2020-04-20 22:47:57 +0530

    [Venu]Refactor.Changes in trash Component

--- src/app/Component/trash/trash.component.ts
+++ src/app/Component/trash/trash.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NoteService } from 'src/app/Services/note.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DataSharingService } from 'src/app/Services/datasharing.service';
 
 @Component({
   selector: 'app-trash',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trash.component.scss']
 })
 export class TrashComponent implements OnInit {
+ //msg : String = "row wrap";
+ //noteid=any;
+  constructor(
+    // private noteservice: NoteService,
+    // private datasharing:DataSharingService,
+    // private snackBar : MatSnackBar
 
-  constructor() { }
+  ) { }
 
   ngOnInit() {
+    //this.deleteTrash();
+    //this.datasharing.Message.subscribe(message =>   this.msg = message);
   }
+  // deleteTrash(){    
+  //   this.noteservice.deleteAll(noteid).subscribe(
+  //     data => {
+  //      console.log("data at trash", data);
+  //      this.deleteTrash();
+  //     },
+  //     error => {
+  //       console.log("error at trash", error);
+  //     }
+  //   )
+  // }
 
 }
