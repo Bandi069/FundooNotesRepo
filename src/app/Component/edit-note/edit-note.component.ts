3abc2a164f01a64131591fa18eb585ace408f5ef
Bandi069 <bandivenu89@gmail.com>
2020-04-21 15:19:09 +0530

    [Venu]Refactor.implementation of edit note component

--- src/app/Component/edit-note/edit-note.component.ts
+++ src/app/Component/edit-note/edit-note.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DataSharingService } from 'src/app/Services/datasharing.service';
+import { NoteService } from 'src/app/Services/note.service';
+import { NoteComponent } from '../note/note.component';
 
 @Component({
   selector: 'app-edit-note',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-note.component.scss']
 })
 export class EditNoteComponent implements OnInit {
-
-  constructor() { }
+// note=any;
+color:any;
+  constructor(private noteser:NoteService,
+    private datasharing:DataSharingService,
+    private dialog:MatDialog) { }
 
   ngOnInit() {
   }
-
+  Updatenote(note)
+  {
+    console.log("AllNotes"+note);
+    if(note.isbin!=true)
+    {
+      const Ref=this.dialog.open(NoteComponent,{
+        data:{note:note},
+        panelClass:'custom-dialog-container'
+      });
+      Ref.afterClosed().subscribe(result=>{
+        note=result;
+      });
+    }
+    else{
+      console.log("cont edit");
+    }
+  }
+  deleteReminder(id){
+    this.color.id=id;
+    this.noteser.removeRemainder(this.color).subscribe(Response=>{
+      console.log(Response);
+    })
+    window.location.reload();
+  }
 }
