bdb16036954ef821cf808cd54af053108495a8ca
Bandi069 <bandivenu89@gmail.com>
2020-04-25 03:40:04 +0530

    [Venu]Refactor.Changes in pin component

--- src/app/Component/pin/pin.component.ts
+++ src/app/Component/pin/pin.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NoteService } from 'src/app/Services/note.service';
+import { DataSharingService } from 'src/app/Services/datasharing.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-pin',
@@ -6,12 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pin.component.scss']
 })
 export class PinComponent implements OnInit {
-
+  @Input() getid;
   constructor(
-    private serviceobj:NoteService,
-    private snackbar:MatSnackBar,
-    private datasharing:DataSharingService) { }
+    private noteser:NoteService,
+    private datasharing:DataSharingService,
+    private snackbar:MatSnackBar) { }
   ngOnInit() {
   }
+  pinval(){
+    this.noteser.Ispin(this.getid.id).subscribe((result)=>{ 
+      if(result!=null){
+        this.datasharing.changeMessage(true);
+        this.snackbar .open('Pinned',' ',{duration:2000});
+      }
+    })
+  }
+  unpinval(){
+      this.noteser.Unpin(this.getid.id).subscribe((result)=>{
+        if(result!=null){
+          this.datasharing.changeMessage(true);
+          this.snackbar.open('UnPinned',' ',{duration:2000});
+        }
+      })
+  }
+
 
 }
