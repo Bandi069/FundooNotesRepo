a3a6bac61be7d7d5dedbb4dac77b3163f4ed2279
Bandi069 <bandivenu89@gmail.com>
2020-04-27 01:12:19 +0530

    [venu]Refactor.Implementation of Collaborator

--- src/app/Component/collaborator/collaborator.component.ts
+++ src/app/Component/collaborator/collaborator.component.ts
@@ -2,21 +2,24 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { NoteService } from 'src/app/Services/note.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-
+export interface DialogData {
+  //animal: string;
+  //name: string;
+}
 @Component({
   selector: 'app-collaborator',
   templateUrl: './collaborator.component.html',
   styleUrls: ['./collaborator.component.scss']
 })
 export class CollaboratorComponent implements OnInit {
-  email: any ="";
-  firstName: any ="";
-  lastName: any ="";
+  email: any = "";
+  firstName: any = "";
+  lastName: any = "";
   constructor(public dialogRef: MatDialogRef<CollaboratorComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private noteService: NoteService,
-    private snackBar : MatSnackBar) { }
+    private snackBar: MatSnackBar) { }
   ngOnInit(
-  ){}
+  ) { }
 
 }
