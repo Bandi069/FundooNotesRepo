daa1e82b3be99a854bd643a60cbb7875478be3bf
Bandi069 <bandivenu89@gmail.com>
2020-05-06 18:18:59 +0530

    [Venu]Add.Adding automated ForgotPasswordsteps.cs file

---
+++ SeleniumNUnitTestProject/Steps/ForgotPasswordSteps.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumNUnitTestProject.Steps
+{
+    [Binding]
+    public class ForgotPasswordSteps
+    {
+        IWebDriver driver = new ChromeDriver();
+        [Given(@"I have navigate to Forgotpassword application")]
+        public void GivenIHaveNavigateToForgotpasswordApplication()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--start maximized");
+            driver = new ChromeDriver(chromeOptions);
+            driver.Navigate().GoToUrl("");
+        }
+        
+        [Given(@"I have entetered the (.*)")]
+        
+        public void GivenIHaveEnteteredThe(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I Click on Submit Button")]
+        public void WhenIClickOnSubmitButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I display the login page of my application")]
+        public void ThenIDisplayTheLoginPageOfMyApplication()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
