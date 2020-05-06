129f76ddb1a478a3b5039f1e8b88107784d06b66
Bandi069 <bandivenu89@gmail.com>
2020-05-06 08:40:47 +0530

    [Venu]Add.Adding Register Steps file

---
+++ SeleniumNUnitTestProject/Steps/RegisterSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumNUnitTestProject
+{
+    [Binding]
+    public class RegisterSteps
+    {
+        [Given(@"I have navigate to register application form")]
+        public void GivenIHaveNavigateToRegisterApplicationForm()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on the submit button")]
+        public void WhenIClickOnTheSubmitButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Isee the login page")]
+        public void ThenIseeTheLoginPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
