8cead61682a3b77c90e431be0ee55dec944a4a58
Bandi069 <bandivenu89@gmail.com>
2020-05-06 20:32:33 +0530

    [Venu]Refactor.Implementation of forgotpassword Component

--- src/app/Component/forgot-password/forgot-password.component.ts
+++ src/app/Component/forgot-password/forgot-password.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { AccountService } from 'src/app/Services/account.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { tokenName } from '@angular/compiler';
 
 
 @Component({
@@ -10,11 +13,29 @@ import { FormControl, Validators, FormGroup } from '@angular/forms';
 })
 export class ForgotPasswordComponent implements OnInit {
   ForgotForm: FormGroup;
-  constructor() { }
+  constructor(private service: AccountService,
+    private route: Router,
+    private snackbar: MatSnackBar) { }
   email = new FormControl('', [
-    Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'), ]);
-  
+    Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'),]);
+
   ngOnInit() {
+  }
+  forgotpassword() {
+    if (this.email.value != null) {
+      const forg = {
+        email: this.email.value,
+      };
+      this.service.forgot(forg).subscribe(result => {
+        console.log('result', result);
+        this.snackbar.open('Reset password link sent to registered mail', 'Dismiss', { 'duration': 4000 });
+        token: localStorage.setItem('token', result.toLocaleString());
+      });
+      this.route.navigate(['/login']);
+    }
+    else {
+      this.snackbar.open("Invalid email address", "", { 'duration': 3000 });
+    }
   }
 
 }
