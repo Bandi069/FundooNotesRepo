5a235b1b0c8d17341695142ca958e089ba7c36bf
Bandi069 <bandivenu89@gmail.com>
2020-05-06 08:49:10 +0530

    [Venu]Refactor.Implementation of Register Steps file

--- SeleniumNUnitTestProject/Steps/RegisterSteps.cs
+++ SeleniumNUnitTestProject/Steps/RegisterSteps.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using TechTalk.SpecFlow;
 
 namespace SeleniumNUnitTestProject
@@ -6,22 +8,23 @@ namespace SeleniumNUnitTestProject
     [Binding]
     public class RegisterSteps
     {
+        IWebDriver driver = new ChromeDriver();
         [Given(@"I have navigate to register application form")]
         public void GivenIHaveNavigateToRegisterApplicationForm()
         {
-            ScenarioContext.Current.Pending();
+            driver.Navigate().GoToUrl("http://localhost:4200/register");
         }
         
         [When(@"I click on the submit button")]
         public void WhenIClickOnTheSubmitButton()
         {
-            ScenarioContext.Current.Pending();
+           // ScenarioContext.Current.Pending();
         }
         
         [Then(@"Isee the login page")]
         public void ThenIseeTheLoginPage()
         {
-            ScenarioContext.Current.Pending();
+            ///ScenarioContext.Current.Pending();
         }
     }
 }
