4cf719d8f9d0fe2b903d5125b6c18ddcb692c25d
Bandi069 <bandivenu89@gmail.com>
2020-05-06 18:49:00 +0530

    [Venu]Add.Adding Forgot PAssword Page file

---
+++ SeleniumNUnitTestProject/Pages/ForgotPasswordPage.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using UnitTestProject.Pages;
+
+namespace SeleniumNUnitTestProject.Pages
+{
+    public class ForgotPasswordPage
+    {
+        IWebDriver webDriver;
+        public ForgotPasswordPage(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);
+            this.webDriver = driver;
+        }
+        [FindsBy(How = How.Id, Using = "mat-input-0")]
+        private IWebElement txtEmail;
+        [FindsBy(How = How.ClassName, Using = "mat-raised-button")]
+        private IWebElement forgotbutton;
+        public void Forgot(string Email)
+        {
+            Thread.Sleep(5000);
+            txtEmail.SendKeys(Email);
+        }
+        public LoginPage ClickForgot()
+        {
+            forgotbutton.Submit();
+            return new LoginPage(webDriver);
+        }
+
+    }
+}
