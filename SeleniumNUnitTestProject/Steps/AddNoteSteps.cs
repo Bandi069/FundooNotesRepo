3afbb1b006d5f00c6fc37c517d6ed65e200f2697
Bandi069 <58691538+Bandi069@users.noreply.github.com>
2020-05-09 09:30:31 +0530

    [Venu]Refactor.Modified Files packages

--- SeleniumNUnitTestProject/Steps/AddNoteSteps.cs
+++ SeleniumNUnitTestProject/Steps/AddNoteSteps.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using SeleniumNUnitTestProject.Pages;
 using System;
 using TechTalk.SpecFlow;
 
@@ -9,40 +10,47 @@ namespace SeleniumNUnitTestProject
     public class AddNoteSteps
     {
         IWebDriver driver = new ChromeDriver();
+
         [Given(@"I should navigated to my application")]
         public void GivenIShouldNavigatedToMyApplication()
         {
-           
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://localhost:4200/login");
         }
         
         [Given(@"I type the credentials")]
         public void GivenITypeTheCredentials(Table table)
         {
-            ScenarioContext.Current.Pending();
+            AddNotePage add = new AddNotePage(driver);
+          //  dynamic data = table.CreateDynamicInstance();
+            //add.Login(data.Email, data.Password);
         }
-        
-        [Given(@"I should see the Dashborad page")]
-        public void GivenIShouldSeeTheDashboradPage()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Given(@"I have write (.*) and (.*)")]
-        public void GivenIHaveWriteAnd(string p0, string p1)
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
         [When(@"I click Login Button in the login page")]
         public void WhenIClickLoginButtonInTheLoginPage()
         {
-            ScenarioContext.Current.Pending();
+            AddNotePage add = new AddNotePage(driver);
+            add.Clicklogin();
+        }
+
+        [Given(@"I should see the Dashborad page")]
+        public void GivenIShouldSeeTheDashboradPage()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://localhost:4200/dashboard/note?page=notes");
+        }
+
+        [Given(@"I have write (.*) and (.*)")]
+        public void GivenIHaveWriteAnd(string p0, string p1)
+        {
+            AddNotePage add = new AddNotePage(driver);
+            add.Addnote(p0, p1);
         }
         
         [When(@"I click on the close button")]
         public void WhenIClickOnTheCloseButton()
         {
-            ScenarioContext.Current.Pending();
+            AddNotePage addnote = new AddNotePage(driver);
+            addnote.ClickClose();
         }
     }
 }
