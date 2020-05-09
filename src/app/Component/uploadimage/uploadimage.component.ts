b25b8a68e5c74e045b0588db2b70372dcd89ff97
Bandi069 <58691538+Bandi069@users.noreply.github.com>
2020-05-09 23:41:26 +0530

    [Venu]Add.Adding upload image component

---
+++ src/app/Component/uploadimage/uploadimage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uploadimage',
+  templateUrl: './uploadimage.component.html',
+  styleUrls: ['./uploadimage.component.scss']
+})
+export class UploadimageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
