8fe69b8beb7101fd15d15f2c186abdb2e6e2ec98
Bandi069 <58691538+Bandi069@users.noreply.github.com>
2020-05-09 09:27:48 +0530

    [Venu]Refactor.Modified AddNotePage

--- SeleniumNUnitTestProject/Pages/AddNotePage.cs
+++ SeleniumNUnitTestProject/Pages/AddNotePage.cs
@@ -1,11 +1,65 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace SeleniumNUnitTestProject.Pages
 {
     class AddNotePage
     {
+        IWebDriver webDriver;
+
+        public AddNotePage(IWebDriver webDriver)
+        {
+            PageFactory.InitElements(webDriver, this);
+            this.webDriver = webDriver;
+        }
+
+        [FindsBy(How = How.Id, Using = "mat-input-0")]
+        private IWebElement txtEmail;
+
+        [FindsBy(How = How.Id, Using = "mat-input-1")]
+        private IWebElement txtPassword;
+
+        [FindsBy(How = How.ClassName, Using = "mat-raised-button")]
+        private IWebElement buttonLogin;
+
+        [FindsBy(How = How.Id, Using = "closeCard")]
+        private IWebElement txtMatCard;
+
+        [FindsBy(How = How.Id, Using = "titleId")]
+        private IWebElement txtTitle;
+
+        [FindsBy(How = How.Id, Using = "takeAnote")]
+        private IWebElement txtDescription;
+
+        [FindsBy(How = How.Id, Using = "close")]
+        private IWebElement buttonClose;
+        public void Login(string Email, string Password)
+        {
+           Thread.Sleep(5000);
+            txtEmail.SendKeys(Email);
+            txtPassword.SendKeys(Password);
+        }
+
+        public void Clicklogin()
+        {
+            buttonLogin.Click();
+        }
+        public void Addnote(string Title, string Description)
+        {
+            Thread.Sleep(4000);
+            txtMatCard.Click();
+            txtTitle.SendKeys(Title);
+            txtDescription.SendKeys(Description);
+        }
+        public DashboardPage ClickClose()
+        {
+            buttonClose.Submit();
+            return new DashboardPage(webDriver);
+        }
 
     }
 }
