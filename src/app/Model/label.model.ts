b7c3c930883b16e591e9eb1c092a99c9fa3a8512
Bandi069 <58691538+Bandi069@users.noreply.github.com>
2020-05-09 23:42:03 +0530

    [Venu]Add.Adding Model folder ts files

---
+++ src/app/Model/label.model.ts
@@ -0,0 +1,6 @@
+export class Label{
+    labelId:number;
+    noteId:number;
+    email:number;
+    labelName:string;
+}
---
+++ src/app/Model/notes.model.ts
@@ -0,0 +1,15 @@
+export class Note {
+    labelname: string;
+    title: String;
+    description: String;
+    changeColor: String;
+    ispinned: number;
+    istrash: number;
+    archive: number;
+    reminder: string;
+    collaborator: string;
+    search: number;
+    profilePic: string;
+    id: number;
+    email: string;
+}
