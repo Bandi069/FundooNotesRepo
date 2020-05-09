35be0252d8ba36b1479f021849aefeeab4cf9cc1
Bandi069 <58691538+Bandi069@users.noreply.github.com>
2020-05-09 07:03:32 +0530

    [Venu]Add. Adding AddNote Step File

---
+++ SeleniumNUnitTestProject/Steps/AddNoteSteps.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumNUnitTestProject
+{
+    [Binding]
+    public class AddNoteSteps
+    {
+        IWebDriver driver = new ChromeDriver();
+        [Given(@"I should navigated to my application")]
+        public void GivenIShouldNavigatedToMyApplication()
+        {
+           
+        }
+        
+        [Given(@"I type the credentials")]
+        public void GivenITypeTheCredentials(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I should see the Dashborad page")]
+        public void GivenIShouldSeeTheDashboradPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have write (.*) and (.*)")]
+        public void GivenIHaveWriteAnd(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click Login Button in the login page")]
+        public void WhenIClickLoginButtonInTheLoginPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on the close button")]
+        public void WhenIClickOnTheCloseButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
