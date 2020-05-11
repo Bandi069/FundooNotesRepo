3643614bfb35920560dcbcedc8972a71486b0b9d
Bandi069 <58691538+Bandi069@users.noreply.github.com>
2020-05-11 17:33:49 +0530

    [Venu]Add.Adding login page and step file

---
+++ BookSwagonTesting/Pages/LoginPage.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookSwagonTesting.Pages
+{
+    class LoginPage
+    {
+        IWebDriver webDriver;
+        public LoginPage(IWebDriver webDriver)
+        {
+            PageFactory.InitElements(webDriver,this);
+            this.webDriver = webDriver;
+        }
+
+    }
+}
---
+++ BookSwagonTesting/Steps/LoginSteps.cs
@@ -0,0 +1,39 @@
+﻿using BookSwagonTesting.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace BookSwagonTesting
+{
+    [Binding]
+    public class LoginSteps
+    {
+        IWebDriver driver = new ChromeDriver(0);
+        [Given(@"I have navigate to my login application")]
+        public void GivenIHaveNavigateToMyLoginApplication()
+        {
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.bookswagon.com/login");
+        }
+        
+        [Given(@"I enter details (.*) and (.*)")]
+        public void GivenIEnterDetailsAnd(string p0, string p1)
+        {
+            LoginPage loginPage = new LoginPage();
+
+        }
+        
+        [When(@"I click on login button")]
+        public void WhenIClickOnLoginButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should see the dashboard page")]
+        public void ThenIShouldSeeTheDashboardPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
