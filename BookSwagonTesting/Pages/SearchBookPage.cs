4cd3a05238366982a770d9aecd44561f588267b5
Bandi069 <58691538+Bandi069@users.noreply.github.com>
2020-05-12 09:20:29 +0530

    [Venu]Add.Adding Search box cs file

---
+++ BookSwagonTesting/Pages/SearchBookPage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookSwagonTesting.Pages
+{
+    class SearchBookPage
+    {
+        IWebDriver driver;
+        public SearchBookPage(IWebDriver webDriver)
+        {
+            PageFactory.InitElements(webDriver, this);
+            this.driver = webDriver;
+        }
+    }
+}
