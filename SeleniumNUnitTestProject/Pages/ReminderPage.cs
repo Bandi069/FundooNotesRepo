64dbaebc6bab0646b0328602db7d9bb3b04e888b
Bandi069 <58691538+Bandi069@users.noreply.github.com>
2020-05-13 07:48:18 +0530

    [Venu]add.Implementation of Reminders PAge and step file

---
+++ SeleniumNUnitTestProject/Pages/ReminderPage.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace SeleniumNUnitTestProject.Pages
+{
+    class ReminderPage
+    {
+        IWebDriver webDriver;
+        public ReminderPage(IWebDriver currentDriver)
+        {
+            PageFactory.InitElements(currentDriver, this);
+            this.webDriver = currentDriver;
+        }
+
+        [FindsBy(How = How.Id, Using ="reminder")]
+        private IWebElement txtreminder;
+
+        [FindsBy(How = How.Id, Using = "select")]
+        private IWebElement txtselect;
+        [FindsBy(How = How.ClassName, Using = "date")]
+        private IWebElement txtdate;
+        [FindsBy(How = How.ClassName, Using = "time")]
+        private IWebElement txttime;
+        [FindsBy(How = How.ClassName, Using = "btnsave")]
+        private IWebElement btndatesave;
+
+        public void reminder()
+        {
+            txtreminder.Click();
+        }
+        public void select()
+        {
+            txtselect.Click();
+        }
+        public void date(string Date,string Time)
+        {
+            Thread.Sleep(5000);
+            txtdate.SendKeys(Date);
+            txttime.SendKeys(Time);
+
+        }
+        public DashboardPage clicksave()
+        {
+            btndatesave.Submit();
+            return new DashboardPage(webDriver);
+        }
+
+    }
+}
---
+++ SeleniumNUnitTestProject/Steps/ReminderSteps.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumNUnitTestProject.Pages;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumNUnitTestProject.Steps
+{
+    [Binding]
+    class ReminderSteps
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [Given(@"I should navigate to displaynote Application")]
+        public void GivenIShouldNavigateToDisplaynoteApplication()
+        {
+            ReminderPage reminderPage = new ReminderPage(driver);
+            reminderPage.reminder();
+        }
+
+        [Given(@"I have click the Reminder icon")]
+        public void GivenIHaveClickTheReminderIcon()
+        {
+            ReminderPage reminderPage = new ReminderPage(driver);
+            reminderPage.select();
+        }
+
+        [When(@"I have the Select date and time")]
+        public void WhenIHaveTheSelectDateAndTime()
+        {
+            ReminderPage reminderPage = new ReminderPage(driver);
+            reminderPage.select();
+        }
+
+        [Then(@"I have fill the values (.*) and (.*)")]
+        public void ThenIHaveFillTheValuesAnd(string p0, string p1)
+        {
+            ReminderPage reminderPage = new ReminderPage(driver);
+            reminderPage.date(p0, p1);
+        }
+
+        [Then(@"I click on the save button")]
+        public void ThenIClickOnTheSaveButton()
+        {
+            ReminderPage reminderPage = new ReminderPage(driver);
+            reminderPage.clicksave();
+        }
+    }
+}
