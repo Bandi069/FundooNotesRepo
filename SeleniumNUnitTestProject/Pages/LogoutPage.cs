1002c2217e7654d7b9f0b6f11860454052d9a962
Bandi069 <58691538+Bandi069@users.noreply.github.com>
2020-05-13 07:54:26 +0530

    [Venu]Refactor.Modified logoutPage file

--- SeleniumNUnitTestProject/Pages/LogoutPage.cs
+++ SeleniumNUnitTestProject/Pages/LogoutPage.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
+using UnitTestProject.Pages;
 
 namespace SeleniumNUnitTestProject.Pages
 {
-   public class LogoutPage
+    class LogoutPage
     {
+        IWebDriver webDriver;
+        public LogoutPage(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);
+            this.webDriver = driver;
+        }
+        [FindsBy(How = How.Id, Using = "btnlogout")]
+        private IWebElement btnlogout;
+
+        public LoginPage ClickSignout()
+        {
+            btnlogout.Click();
+            return new LoginPage(webDriver);
+        }
     }
 }
