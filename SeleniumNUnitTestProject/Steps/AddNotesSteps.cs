da81b6fd4b57f308b4f798fb62d51eeb9caf7b1f
Bandi069 <58691538+Bandi069@users.noreply.github.com>
2020-05-13 07:45:24 +0530

    [Venu]Add.Implementation of Addnote steps file

---
+++ SeleniumNUnitTestProject/Steps/AddNotesSteps.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumNUnitTestProject.Pages;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumNUnitTestProject
+{
+    [Binding]
+    public class AddNotesSteps
+    {
+        IWebDriver currentDriver = new ChromeDriver();
+
+        [Given(@"I type the datas")]
+        public void GivenITypeTheDatas(Table table)
+        {
+           // ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I should see  Dashborad page")]
+        public void GivenIShouldSeeDashboradPage()
+        {
+            currentDriver.Manage().Window.Maximize();
+            currentDriver.Navigate().GoToUrl("http://localhost:4200/dashboard/note");
+        }
+
+        [Given(@"I have put (.*) and (.*)")]
+        public void GivenIHavePutAnd(string p0, string p1)
+        {
+            AddNotePage note = new AddNotePage(currentDriver);
+            note.Addnote(p0,p1);
+        }
+
+        [When(@"I click close button")]
+        public void WhenIClickCloseButton()
+        {
+            AddNotePage note = new AddNotePage(currentDriver);
+            note.ClickClose();
+
+        }
+
+    }
+}
