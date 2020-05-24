43e0b5f0357ef13a8c6d684fbc538a9777c823c9
Bandi069 <58691538+Bandi069@users.noreply.github.com>
2020-05-24 16:42:40 +0530

    [Venu]Refactor.Changes in pick reminder component

--- src/app/Component/pickreminder/pickreminder.component.ts
+++ src/app/Component/pickreminder/pickreminder.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, Output, EventEmitter } from '@angular/core';
 //import { DialogData } from '../editlabel/editlabel.component';
+import { FormControl, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NoteService } from 'src/app/Services/note.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 export interface DialogData {
-  
+
 }
 @Component({
   selector: 'app-pickreminder',
@@ -11,9 +14,20 @@ export interface DialogData {
 })
 export class PickreminderComponent implements OnInit {
 
-  constructor( 
-    @Inject(MAT_DIALOG_DATA) public data:any   ) { }
-    
+  constructor(
+    private noteServices: NoteService,
+    private snackBar: MatSnackBar,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+  date = new FormControl('');
+  time = new FormControl('');
+  // @Output() reminderEventForCards = new EventEmitter<any>();
+  //@Output() reminderEvent = new EventEmitter<any>();
   ngOnInit() {
+  }
+  savefun() {
+    // const reminderData = this.date.value.toLocaleDateString() + "," + " " + this.time.value
+    // console.log("reminder: ", reminderData)
+    // this.reminderEvent.emit(reminderData);
+    // this.reminderEventForCards.emit(reminderData);
   }
 }
