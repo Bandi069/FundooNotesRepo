180111d019705a13263a292123a13ca1332091f5
Bandi069 <58691538+Bandi069@users.noreply.github.com>
2020-05-24 16:46:30 +0530

    [Venu]Refactor.Changes in collaborator component

--- src/app/Component/collaborator/collaborator.component.ts
+++ src/app/Component/collaborator/collaborator.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { NoteService } from 'src/app/Services/note.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormControl } from '@angular/forms';
 export interface DialogData {
   //animal: string;
   //name: string;
@@ -15,6 +16,9 @@ export class CollaboratorComponent implements OnInit {
   email: any = "";
   firstName: any = "";
   lastName: any = "";
+  count: Number = 0;
+  noteData : any ;
+  collabData : any;
   constructor(
     public dialogRef: MatDialogRef<CollaboratorComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
@@ -25,4 +29,24 @@ export class CollaboratorComponent implements OnInit {
   addCollaborator(){
     
   }
+  deletecollaborator(note){
+   
+  }
+  //  key = new FormControl();
+  // options: any = "";
+
+  // countingcollab(count) {
+  //   if (count > 2 && this.key.value) {
+  //     this.noteService.(this.key.value).subscribe(
+  //       data => {
+  //         console.log("received  ", data);
+  //         this.options = (data as any).message;
+  //       },
+  //       error => {
+  //         console.log("received  ", error);
+  //       }
+  //     )
+  //   }
+  // }
+  
 }
